a13279dbd03c124703abb6fb02883a4b37c8b567
pratiktupe14 <pratikdiliptupe14@gmail.com>
2026-08-31 01:25:47 +0530

    Fix Vercel serverless function entry point require paths for NFT bundler static resolution

--- api/index.js
+++ api/index.js
@@ -1,26 +1,5 @@
-const path = require('path');
-
-let app;
-try {
-  app = require('../backend/src/app');
-} catch (e1) {
-  try {
-    app = require('./backend/src/app');
-  } catch (e2) {
-    app = require('./src/app');
-  }
-}
-
-let initDatabase;
-try {
-  initDatabase = require('../backend/src/config/db').initDatabase;
-} catch (e1) {
-  try {
-    initDatabase = require('./backend/src/config/db').initDatabase;
-  } catch (e2) {
-    initDatabase = require('./src/config/db').initDatabase;
-  }
-}
+const app = require('../backend/src/app');
+const { initDatabase } = require('../backend/src/config/db');
 
 if (initDatabase) {
   initDatabase().catch(err => console.warn('[SERVERLESS INIT NOTE]', err.message));
